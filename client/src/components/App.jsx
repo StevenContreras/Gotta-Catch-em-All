adb172c5757cbe372459044dabfae988bf8c9153
Steven Contreras <SFContra@gmail.com>
2019-12-05 10:11:41 -0800

    initial set up with server to react rendering

---
+++ client/src/components/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import axios from 'axios';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <div>
+        <div>
+          Happy Hanukkah from React!
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
