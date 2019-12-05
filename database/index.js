337e753542cb31eaafbb2a955672624b5e8cb22e
Steven Contreras <SFContra@gmail.com>
2019-12-05 13:48:59 -0800

    complete database test seed

--- database/index.js
+++ database/index.js
@@ -1,4 +1,12 @@
 const mysql = require('mysql');
 const mysqlConfig = require('./config.js');
 
-const connection = mysql.createConnection(mysqlConfig);
+const db = mysql.createConnection(mysqlConfig);
+
+db.connect((err) => {
+  if (err) {
+    console.error("Not Connected to Database")
+  } else {
+    console.log("Connected to Database!")
+  }
+})
--- database/schema.sql
+++ database/schema.sql
@@ -1,12 +1,19 @@
+DROP DATABASE IF EXISTS pokemon;
+
 CREATE DATABASE pokemon;
 
 USE pokemon;
 
 CREATE TABLE team (
   id int NOT NULL AUTO_INCREMENT,
-  name varchar(20) NOT NULL,
-  number int NOT NULL,
-  type varchar(20) NOT NULL,
-  image LONGBLOB NOT NULL,
-  PRIMARY KEY (ID)
-)
+  nombre varchar(20) NOT NULL,
+  numero int NOT NULL,
+  tipo varchar(20) NOT NULL,
+  amount int NOT NULL,
+  img LONGBLOB NOT NULL,
+  PRIMARY KEY (ID),
+  CONSTRAINT UC_team UNIQUE (id, nombre, numero)
+);
+
+INSERT INTO team (nombre, numero, tipo, amount, img) VALUES("bulbasaur", 1, "poison, grass", 1, "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png");
+INSERT INTO team (nombre, numero, tipo, amount, img) VALUES("pikachu", 25, "electric", 1, "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png")
