fe24b02ae8b5215d3187f882007965049c683f8d
Steven Contreras <SFContra@gmail.com>
2019-12-05 20:38:51 -0800

    complete onClicke even to random pokemon

--- client/src/components/RandomEncounter.jsx
+++ client/src/components/RandomEncounter.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 const RandomEncounter = ({random}) => (
     <div>
       <h2>Random Attack!</h2>
-      <h3>{random.name}</h3>
-      <h4>Poke# {random.id}</h4>
-      <h4>Type: {random.type}</h4>
-      <img src={random.img}/>
+      <button onClick={() => (console.log("Clicked"))}>
+        <h3>{random.name}</h3>
+        <h4>Poke# {random.id}</h4>
+        <h4>Type: {random.type}</h4>
+        <img src={random.img}/>
+      </button>
     </div>
 )
 
